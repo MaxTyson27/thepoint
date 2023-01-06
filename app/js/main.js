800233d519f7df28c18bd4e28175d5323b4c836b
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2023-01-06 14:12:35 +0300

    adad

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,24 @@
 $(function () {
-  $('.content__slider').slick({
+  $(".content__slider").slick({
     dots: true,
     fade: true,
-  })
-  $('.gallery__slider').slick({
+    adaptiveHeight: true,
+  });
+  $(".gallery__slider").slick({
     dots: true,
     fade: true,
-  })
-  $('.name').keypress(function (e) {
+  });
+  $(".name").keypress(function (e) {
     if (e.keyCode == 32) return false;
     if (this.value.length == 1) {
       this.value = this.value.toUpperCase();
     }
   });
-  $('.phone').inputmask({ "mask": "+7(999) 999-99-99" });
+  $(".phone").inputmask({ mask: "+7(999) 999-99-99" });
 
-  $('.logo').on('click', e => {
-    e.preventDefault()
-  })
+  $(".logo").on("click", (e) => {
+    e.preventDefault();
+  });
 
   $("form").submit(function (event) {
     event.preventDefault();
@@ -29,7 +30,7 @@ $(function () {
         data: str,
         success: function () {
           console.log("send");
-          $('.thank').addClass('active')
+          $(".thank").addClass("active");
           $("form")[0].reset();
         },
         error: function () {
@@ -37,204 +38,187 @@ $(function () {
         },
       });
     } else {
-      $('.error').addClass('active')
+      $(".error").addClass("active");
     }
   });
-
 });
 
-
 const validateRadio = () => {
-
-  let valid = false
+  let valid = false;
 
   const checkedAllRadio = {
     project: false,
     size: false,
     price: false,
     agent: false,
-  }
+  };
 
   const radioElems = {
     project: [...document.querySelectorAll(`input[name='project'`)],
     size: [...document.querySelectorAll(`input[name='size'`)],
     price: [...document.querySelectorAll(`input[name='price'`)],
     agent: [...document.querySelectorAll(`input[name='agent'`)],
-  }
+  };
 
-  for(let key in radioElems){
-    radioElems[key].forEach( radio => {
+  for (let key in radioElems) {
+    radioElems[key].forEach((radio) => {
       switch (radio.checked) {
         case true:
-          checkedAllRadio[key] = !checkedAllRadio[key]
+          checkedAllRadio[key] = !checkedAllRadio[key];
           break;
-      
+
         default:
           break;
       }
-
     });
   }
 
-  const isCheckedAllRadioArray = Object.keys(checkedAllRadio).map(key => {
-    return checkedAllRadio[key]
-  })
+  const isCheckedAllRadioArray = Object.keys(checkedAllRadio).map((key) => {
+    return checkedAllRadio[key];
+  });
 
-  const isCheckedAllRadio = isCheckedAllRadioArray.every(bool => {
-    return bool
-  })
+  const isCheckedAllRadio = isCheckedAllRadioArray.every((bool) => {
+    return bool;
+  });
 
-  if(isCheckedAllRadio){
-    valid = true
+  if (isCheckedAllRadio) {
+    valid = true;
   }
 
-  return valid
-
-
-}
-
+  return valid;
+};
 
 const validateInputs = () => {
-  const inputName = document.querySelector('.name')
-  inputName.addEventListener('input', e => {
-    e.target.value = e.target.value.replace(/[0-9]/g, '');
-  })
-  
-}
+  const inputName = document.querySelector(".name");
+  inputName.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[0-9]/g, "");
+  });
+};
 
 const animationContent = () => {
   const animatePromise = (element, className, t, deleteClass) => {
     return new Promise((resolve) => {
       setTimeout(() => {
-        const findedElem = document.querySelector(element)
-          if(!deleteClass){
-            findedElem.classList.add(className)
-          } else {
-            findedElem.classList.remove(className)
-          } 
-          resolve('123')
+        const findedElem = document.querySelector(element);
+        if (!deleteClass) {
+          findedElem.classList.add(className);
+        } else {
+          findedElem.classList.remove(className);
+        }
+        resolve("123");
       }, t);
-    })
-  }
+    });
+  };
 
-
-
-  animatePromise('.logo', 'anim', 1000, false).then((res) => {
-
-    return animatePromise('.line', 'anim', 500, false).then(res => {
-
-      return animatePromise('.line', 'hidden', 1000, false).then(res => {
-        
-        return animatePromise('.content__copy', 'anim', 500, false).then(res => {
-
-          return animatePromise('.logo', 'anim2', 1000, false).then(res => {
-
-            return animatePromise('.content__title', 'anim', 1000, false).then(res => {
-
-              return animatePromise('.logo', 'anim', 0, true).then(res => {
-
-                return animatePromise('.content__copy', 'anim', 0, true).then(res => {
-
-                  return animatePromise('.wrapper', 'load', 500, true)
-                })
-              })            
-            })
-          })
-         })
-      })
-    })
-  })
-
-}
+  animatePromise(".logo", "anim", 1000, false).then((res) => {
+    return animatePromise(".line", "anim", 500, false).then((res) => {
+      return animatePromise(".line", "hidden", 1000, false).then((res) => {
+        return animatePromise(".content__copy", "anim", 500, false).then(
+          (res) => {
+            return animatePromise(".logo", "anim2", 1000, false).then((res) => {
+              return animatePromise(
+                ".content__title",
+                "anim",
+                1000,
+                false
+              ).then((res) => {
+                return animatePromise(".logo", "anim", 0, true).then((res) => {
+                  return animatePromise(".content__copy", "anim", 0, true).then(
+                    (res) => {
+                      return animatePromise(".wrapper", "load", 500, true);
+                    }
+                  );
+                });
+              });
+            });
+          }
+        );
+      });
+    });
+  });
+};
 
 const tabsLogic = () => {
-  const buttons = [...document.querySelectorAll('.button')]
-  const tabs = [...document.querySelectorAll('.tab')]
-  const logo = document.querySelector('.logo')
+  const buttons = [...document.querySelectorAll(".button")];
+  const tabs = [...document.querySelectorAll(".tab")];
+  const logo = document.querySelector(".logo");
 
-  buttons.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-      e.preventDefault()
-      const id = this.getAttribute('href')
-      const elem = document.querySelector(id)
-      tabs.forEach(tab => {
-        tab.classList.add('hidden')
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = this.getAttribute("href");
+      const elem = document.querySelector(id);
+      tabs.forEach((tab) => {
+        tab.classList.add("hidden");
       });
-      elem.classList.remove('hidden')
-      if(id === '#gallery'){
-        logo.classList.add('small')
+      elem.classList.remove("hidden");
+      if (id === "#gallery") {
+        logo.classList.add("small");
       } else {
-        logo.classList.remove('small')
+        logo.classList.remove("small");
       }
-      if(id === '#contact'){
-        document.querySelector('.logo').style.display = 'none'
-        if(document.documentElement.offsetWidth <= 1000){
-          document.querySelector('.wrapper').style.height = '100%'
+      if (id === "#contact") {
+        document.querySelector(".logo").style.display = "none";
+        if (document.documentElement.offsetWidth <= 1000) {
+          document.querySelector(".wrapper").style.height = "100%";
         }
       }
-    })
+    });
   });
-
-
-}
+};
 
 const changeLang = () => {
-  const buttons = [...document.querySelectorAll('.lang')]
-
+  const buttons = [...document.querySelectorAll(".lang")];
 
   const hiddenBlocks = (language) => {
-    const elemsRu = [...document.querySelectorAll('.ru')]
-    const elemsEn = [...document.querySelectorAll('.en')]
-    if(language === 'ru'){
-      elemsRu.forEach(elem => {
-        elem.style.display = 'block'
+    const elemsRu = [...document.querySelectorAll(".ru")];
+    const elemsEn = [...document.querySelectorAll(".en")];
+    if (language === "ru") {
+      elemsRu.forEach((elem) => {
+        elem.style.display = "block";
       });
-      elemsEn.forEach(elem => {
-        elem.style.display = 'none'
+      elemsEn.forEach((elem) => {
+        elem.style.display = "none";
       });
     } else {
-      elemsRu.forEach(elem => {
-        elem.style.display = 'none'
+      elemsRu.forEach((elem) => {
+        elem.style.display = "none";
       });
-      elemsEn.forEach(elem => {
-        elem.style.display = 'block'
+      elemsEn.forEach((elem) => {
+        elem.style.display = "block";
       });
     }
-  }
+  };
 
-  buttons.forEach(button => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const lang = e.target.getAttribute("data-lang");
+      const buttonsRu = [...document.querySelectorAll('[data-lang="ru"]')];
+      const buttonsEn = [...document.querySelectorAll('[data-lang="en"]')];
 
-    button.addEventListener('click', e => {
-
-      e.preventDefault()
-      const lang = e.target.getAttribute('data-lang')
-      const buttonsRu = [...document.querySelectorAll('[data-lang="ru"]')]
-      const buttonsEn = [...document.querySelectorAll('[data-lang="en"]')]
-
-      if(lang === 'ru'){
-        buttonsEn.forEach(btn => {
-          btn.classList.remove('active')
+      if (lang === "ru") {
+        buttonsEn.forEach((btn) => {
+          btn.classList.remove("active");
         });
-        buttonsRu.forEach(btn => {
-          btn.classList.add('active')
+        buttonsRu.forEach((btn) => {
+          btn.classList.add("active");
         });
       } else {
-        buttonsEn.forEach(btn => {
-          btn.classList.add('active')
+        buttonsEn.forEach((btn) => {
+          btn.classList.add("active");
         });
-        buttonsRu.forEach(btn => {
-          btn.classList.remove('active')
+        buttonsRu.forEach((btn) => {
+          btn.classList.remove("active");
         });
       }
 
-      e.target.classList.add('active')
+      e.target.classList.add("active");
 
-
-      hiddenBlocks(lang)
-
-    })
+      hiddenBlocks(lang);
+    });
   });
-}
+};
 
 // const setPass = () => {
 //   const pass = '1122'
@@ -244,7 +228,7 @@ const changeLang = () => {
 //   }
 // }
 // setPass()
-changeLang()
-validateInputs()
-tabsLogic()
-animationContent()
+changeLang();
+validateInputs();
+tabsLogic();
+animationContent();
--- app/send.php
+++ app/send.php
@@ -14,15 +14,19 @@ $r4 = $_POST['agent'];
  $mail->SMTPDebug = 1;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'noreply-nedvex@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'aAJjV7BKfRECqAZdBvG7'; // Ваш пароль от почты с которой будут отправляться письма aAJjV7BKfRECqAZdBvG7 -pR1IOoIyuo1
+$mail->Username = 'no-reply@nedvex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'eqpxaayaktuczbty'; // Ваш пароль от почты с которой будут отправляться письма aAJjV7BKfRECqAZdBvG7 -pR1IOoIyuo1
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('noreply-nedvex@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('maxmaximsirotkin@mail.ru');       // Кому будет уходить письмо
+$mail->setFrom('no-reply@nedvex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('A.sokolov@nedvex.ru');       // Кому будет уходить письмо
+$mail->addAddress('O.rechkina@nedvex.ru');       // Кому будет уходить письмо
+$mail->addAddress('E.parfenova@nedvex.ru');       // Кому будет уходить письмо
+$mail->addAddress('r.galstyan@nedvex.ru');       // Кому будет уходить письмо
+$mail->addAddress('i.pomigueva@apdevelopment.ru');       // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -31,8 +35,73 @@ $mail->addAddress('maxmaximsirotkin@mail.ru');       // Кому будет ух
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
+$message = '
+<html>
+<body>
+<center>
+<table border="1" cellpadding="6" cellspacing="0" width="90%" bordercolor="#DBDBDB">
+<tr><td colspan="2" align="center" bgcolor="#E4E4E4"><b>Информация о клиенте</b></td></tr>
+<tr>
+  <td>
+    <b>Как вы узнали о нашем проекте?</b>
+  </td>
+  <td>
+    '. $r1 .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Размер квартиры, который вас интересует?</b>
+  </td>
+  <td>
+    '. $r2 .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Бюджет</b>
+  </td>
+  <td>
+    '. $r3 .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Являетесь ли вы агентом недвижимости?</b>
+  </td>
+  <td>
+    '. $r4 .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Имя</b>
+  </td>
+  <td>
+    '. $name .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Почта</b>
+  </td>
+  <td>
+    '. $email .'
+  </td>
+</tr>
+<tr>
+  <td>
+    <b>Телефон</b>
+  </td>
+  <td>
+    '. $phone .'
+  </td>
+</tr>
+';
+
 $mail->Subject = 'Заявка с сайта The-Point.ru';
-$mail->Body    = "Как вы узнали о нашем проекте?: ".$r1."\nРазмер квартиры, который вас интересует: ".$r2."\nБюджет: ".$r3."\nЯвляетесь ли вы агентом недвижимости?: ".$r4."\nПочта: ".$email."\nИмя: ".$name."\nТелефон: ".$phone;
+// $mail->Body    = "Как вы узнали о нашем проекте?: ".$r1."\nРазмер квартиры, который вас интересует: ".$r2."\nБюджет: ".$r3."\nЯвляетесь ли вы агентом недвижимости?: ".$r4."\nПочта: ".$email."\nИмя: ".$name."\nТелефон: ".$phone;
+$mail->Body = $message;
 $mail->AltBody = '';
 
 try {
